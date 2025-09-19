eae3b270d64bb0a8975448ced2108f6ddc1af7fd
Prasad <prasad.shalika@empirindustry.com>
2025-09-19 15:18:59 +0530

    change vue

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'url'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/template-vue/',
-  plugins: [
-    vue()
-  ]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
